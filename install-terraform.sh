9e888a46ed58596dbb15f7f5b937837efe8cb7d6
Valentin Siryk <valentinsiryk@gmail.com>
2019-02-26 15:08:47 +0200

    improved terraform installation script

--- install-terraform.sh
+++ install-terraform.sh
@@ -3,20 +3,16 @@
 # Arguments:
 #   [terraform_version]    - will be used latest if not set
 
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 if [ $UID -ne 0 ]; then
     echo "Expected run this script as root user!"
     exit 1
 fi
 
 APP_VERSION="$1"
-
 APP_NAME='terraform'
 
 APP_BIN_PATH="/usr/bin/$APP_NAME"
 APP_TMP_DIR=$(mktemp -d)
-
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 
 function cleanup() {
@@ -24,12 +20,35 @@ function cleanup() {
 }
 trap cleanup EXIT
 
+
+function check_current_app_bin_path() {
+    CURRENT_APP_BIN_PATH=$(which $APP_NAME)
+    if [ -n "$CURRENT_APP_BIN_PATH" ]; then
+        APP_BIN_PATH=$CURRENT_APP_BIN_PATH
+    fi
+}
+
+function get_latest_version_number() {  
+    APP_VERSION=$(curl https://releases.hashicorp.com/index.json 2>/dev/null | jq "{$APP_NAME}" \
+                    | egrep "${OS}.*amd64" | grep -v "\-alpha\|\-rc\|\-beta" \
+                    | sort --version-sort -r | head -1 | awk -F[_] '{print $2}')
+}
+
+function exit_if_installed() {
+    if which $APP_NAME >/dev/null; then
+        CURRENT_APP_VERSION="$($APP_NAME -v | awk -F'v' '{print $2}' | head -n 1)"
+
+        if [ "$CURRENT_APP_VERSION" == "${APP_VERSION}" ]; then
+            echo "Already installed actual $APP_NAME version $CURRENT_APP_VERSION"
+            exit 0
+        fi
+
+        echo -e "Current $APP_NAME version: $CURRENT_APP_VERSION\nWill be installed version: $APP_VERSION"
+    fi
+}
+
 function install_app() {
     APP_URL="https://releases.hashicorp.com/$APP_NAME/$APP_VERSION/${APP_NAME}_${APP_VERSION}_${OS}_amd64.zip"
-
-    if ! which curl 2>&1 >/dev/null; then
-        apt-get install -y --no-install-recommends curl >/dev/null
-    fi
 
     cd $APP_TMP_DIR
     echo "Downloading ${APP_URL}..."
@@ -44,41 +63,12 @@ function install_app() {
     echo "The $APP_NAME $APP_VERSION was installed"
 }
 
-CURRENT_APP_BIN_PATH=$(which $APP_NAME)
-if [ -n "$CURRENT_APP_BIN_PATH" ]; then
-    APP_BIN_PATH=$CURRENT_APP_BIN_PATH
-fi
-
-apt-get update >/dev/null
+check_current_app_bin_path
 
 if [ -z "$APP_VERSION" ]; then
-    if ! which jq 2>&1 >/dev/null; then
-        apt-get install -y --no-install-recommends jq >/dev/null
-    fi
-
-    if ! which curl 2>&1 >/dev/null; then
-        apt-get install -y --no-install-recommends curl >/dev/null
-    fi
-
-    APP_VERSION=$(curl https://releases.hashicorp.com/index.json 2>/dev/null \
-                    | jq "{$APP_NAME}" \
-                    | egrep "${OS}.*amd64" \
-                    | grep -v "\-alpha\|\-rc\|\-beta" \
-                    | sort --version-sort -r \
-                    | head -1 \
-                    | awk -F[_] '{print $2}')
+    get_latest_version_number
 fi
 
-if which $APP_NAME >/dev/null; then
-    CURRENT_APP_VERSION="$($APP_NAME -v | awk -F'v' '{print $2}' | head -n 1)"
-
-    if [ "$CURRENT_APP_VERSION" == "${APP_VERSION}" ]; then
-        echo "Already installed actual $APP_NAME version $CURRENT_APP_VERSION"
-        exit 0
-    fi
-
-    echo -e "Current $APP_NAME version: $CURRENT_APP_VERSION\nWill be installed version: $APP_VERSION"
-fi
-
+exit_if_installed
 install_app
 
